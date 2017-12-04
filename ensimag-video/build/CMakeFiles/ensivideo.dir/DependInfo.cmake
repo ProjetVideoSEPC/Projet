46ec3e8780386e84a31d06d1a8b988d8379402b8
Dijacks <bastien.bagliotto@phelma.grenoble-inp.fr>
2017-12-04 17:51:22 +0100

    first

---
+++ ensimag-video/build/CMakeFiles/ensivideo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bast/Documents/sepc/ensimag-video/src/ensitheora.c" "/home/bast/Documents/sepc/ensimag-video/build/CMakeFiles/ensivideo.dir/src/ensitheora.c.o"
+  "/home/bast/Documents/sepc/ensimag-video/src/ensivideo.c" "/home/bast/Documents/sepc/ensimag-video/build/CMakeFiles/ensivideo.dir/src/ensivideo.c.o"
+  "/home/bast/Documents/sepc/ensimag-video/src/ensivorbis.c" "/home/bast/Documents/sepc/ensimag-video/build/CMakeFiles/ensivideo.dir/src/ensivorbis.c.o"
+  "/home/bast/Documents/sepc/ensimag-video/src/oggstream.c" "/home/bast/Documents/sepc/ensimag-video/build/CMakeFiles/ensivideo.dir/src/oggstream.c.o"
+  "/home/bast/Documents/sepc/ensimag-video/src/stream_common.c" "/home/bast/Documents/sepc/ensimag-video/build/CMakeFiles/ensivideo.dir/src/stream_common.c.o"
+  "/home/bast/Documents/sepc/ensimag-video/src/synchro.c" "/home/bast/Documents/sepc/ensimag-video/build/CMakeFiles/ensivideo.dir/src/synchro.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ensimag-video/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bast/Documents/sepc/ensimag-video
+# Build directory: /home/bast/Documents/sepc/ensimag-video/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CommonPthreadError "perl" "/home/bast/Documents/sepc/ensimag-video/tests/pthcocci.pl")
